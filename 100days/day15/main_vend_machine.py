713628a02921f49c884c0553136e6e7131906c15
masterTrooperMx <mastertrooper@gmail.com>
2023-10-12 16:54:44 -0600

    coffee machine example doing great

---
+++ 100days/day15/main_vend_machine.py
@@ -0,0 +1,71 @@
+from vend_machine_data import beverage, stock, coins
+# import only system from os
+import os
+# import call method from subprocess module
+from subprocess import call
+
+# define clear function
+def clear():
+    # check and make call for specific operating system
+    _ = call('clear' if os.name == 'posix' else 'cls')
+
+def fill_machine():
+    stock["milk"] = 200 # ml
+    stock["water"] = 300 # ml
+    stock["coffee"] = 100 # g
+    stock["money"] = 0 # $
+
+def do_report():
+    print('my stock', stock)
+
+def do_quit():
+    print("Good buy then!!")
+
+def ask_money():
+    pennies = int(input("how many pennies? ").lower())
+    nickels = int(input("how many nicles? ").lower())
+    dimes = int(input("how many dimes? ").lower())
+    quarters = int(input("how many quarters? ").lower())
+    return (pennies*coins["penny"] + nickels*coins["nickel"] + dimes*coins["dime"] + quarters*coins["quarter"]);
+
+def get_attr_Bevb(attr, val):
+    return next((drink for drink in beverage if drink[attr] == val), None) # builtin iterator function
+
+def sell_beverage():
+    fill_machine()
+    do_report()
+    game_should_continue = True
+    while game_should_continue:
+        guess = input("what beverage: latte, capuccino, expresso? ").lower()
+        if guess == "report":
+            do_report()
+        elif guess == "quit":
+            do_quit()
+            game_should_continue = False
+        elif guess == "expresso" or guess == "latte" or guess == "capuccino":
+            payment = ask_money()
+            beverage = get_attr_Bevb("name", guess) # lookfor entry with expresso
+            price = beverage["price"]
+            print("Received: ", payment, "Price:", price) # get its price
+            if payment >= price:
+                # check whether we have in stock materials to make beverage
+                if stock["water"] >= beverage["water"] and stock["coffee"] >= beverage["coffee"] and stock["milk"] >= beverage["milk"]:
+                    # discount all stock and add money to  stock
+                    print("Thanks for you buying ", payment-price, "change")
+                    stock["water"] -= beverage["water"]
+                    stock["coffee"] -= beverage["coffee"]
+                    stock["milk"] -= beverage["milk"]
+                    stock["money"] += price
+                else:
+                    print("Not enought material to do beverage")
+                    if stock["water"] < beverage["water"]:
+                        print("not enought water ", stock["water"], beverage["water"])
+                    if stock["coffee"] < beverage["coffee"]: 
+                        print("not enought coffee ", stock["coffee"], beverage["coffee"])
+                    if stock["milk"] < beverage["milk"]:
+                        print("not enought milk ",stock["milk"], beverage["milk"])
+                    print("Refounding money ", payment)
+            else:
+                print("Not enought money, refounding money ", payment)
+
+sell_beverage()
---
+++ 100days/day15/vend_machine_data.py
@@ -0,0 +1,37 @@
+beverage = [
+    {
+        "name": "expresso",
+        "water": 50,
+        "coffee": 18,
+        "milk": 0,
+        "price": 1.5
+    },
+    {
+        "name": "latte",
+        "water": 200,
+        "coffee": 24,
+        "milk": 150,
+        "price": 2.5
+    },
+    {
+        "name": "capuccino",
+        "water": 250,
+        "coffee": 24,
+        "milk": 100,
+        "price": 3.0
+    }
+]
+
+stock = {
+        "water": 300,
+        "coffee": 100,
+        "milk": 200,
+        "money": 0
+    }
+
+coins = {
+    "penny": 0.01,
+    "nickel": 0.05,
+    "dime": 0.1,
+    "quarter": 0.25
+}
