4057355f22ecb62a110bb8243aa0bf5993e954a9
masterTrooperMx <mastertrooper@gmail.com>
2023-11-04 09:42:01 -0600

    crteating align and setup methods for helpers

--- 100days/day19/tests/test_states.py
+++ 100days/day19/tests/test_states.py
@@ -2,10 +2,20 @@
 import sys
 # adding path
 sys.path.insert(0, '/Users/jescruz/dev/PY/PY_notebooks/100days/day19')
-from ttt_race.helpers import screen, timmy_the_turtle, ttt_setup
+from ttt_race.helpers import screen, l_turtles, ttt_setup, ttt_loadlist, ttt_align_players
 # setting up everything
 ttt_setup()
+ttt_loadlist()
+
+""" x = -300
+y = 90
+for timmy in l_turtles:
+    timmy.penup()
+    timmy.goto(x, y)
+    y += 30 """
+ttt_align_players()
+
+#print(l_turtles)
 
 # closing  eveything
-timmy_the_turtle.penup()
 screen.exitonclick()
--- 100days/day19/ttt_race/helpers.py
+++ 100days/day19/ttt_race/helpers.py
@@ -1,14 +1,28 @@
 # different shapes
 from turtle import *
 # import random
-from random import randint
+import random
 
 screen = Screen()
-timmy_the_turtle = Turtle()
+players = 8
+l_colors = ["brown", "black", "red", "blue", "orange", "pink", "cyan", "magenta", "purple", "yellow", "green", "olive"]
+# create a random list of turtles
+l_turtles = []
+
+def ttt_loadlist():
+    for _ in range(players):
+        timmy = Turtle()
+        timmy.shape("turtle")
+        timmy.color(random.choice(l_colors))
+        l_turtles.append(timmy)
 
 def ttt_setup():
     screen.colormode(255)
-    timmy_the_turtle.shape("classic")
-    timmy_the_turtle.color("red")
-    timmy_the_turtle.home()
-    timmy_the_turtle.pendown()
+
+def ttt_align_players():
+    x = -300
+    y = 90
+    for timmy in l_turtles:
+        timmy.penup()
+        timmy.goto(x, y)
+        y += 30
