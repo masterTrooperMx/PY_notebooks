4eb96757e9c9152b4fea80077119f1b1228c6529
masterTrooperMx <mastertrooper@gmail.com>
2023-11-14 07:58:29 -0600

    up, down and so added for new functionality

---
+++ 100days/day21/test/test5.py
@@ -0,0 +1,74 @@
+from turtle import Screen, Turtle
+import time
+
+screen = Screen()
+arr_turtles = []
+keep_running = True
+# constants for heading
+UP    =  90
+DOWN  = 270
+RIGHT = 0
+LEFT  = 180
+
+def setupScreen():
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("My Snake Game")
+    screen.tracer(0)
+
+# create snake body
+def createSnakeBody():
+    for i in range(3):
+        timmy = Turtle(shape="square")
+        timmy.color("white")
+        timmy.penup()
+        timmy.forward(20*i)
+        arr_turtles.append(timmy)
+
+# moving snake
+def turnUp():
+    arr_turtles[0].setheading(UP)
+
+def turnDown():
+    arr_turtles[0].setheading(DOWN)
+
+def turnLeft():
+    arr_turtles[0].setheading(LEFT)
+
+def turnRight():
+    arr_turtles[0].setheading(RIGHT)
+
+screen.listen()
+
+screen.onkey(turnUp, 'Up')
+screen.onkey(turnDown, 'Down')
+screen.onkey(turnLeft, 'Left')
+screen.onkey(turnRight, 'Right')
+
+# do_move
+def doMove():
+    # move snake
+    while keep_running:
+        screen.update()
+        time.sleep(0.1)
+        for t in reversed(arr_turtles):
+            ind = arr_turtles.index(t)
+            timmy = t
+            if ind > 0:
+                timmy_bef = arr_turtles[ind-1]
+                timmy.goto(timmy_bef.pos())
+                #print(f"{ind} {ind-1}")
+            else:
+                #print(f"{ind}-f")
+                timmy.forward(15)
+                #timmy.left(45)
+            #print(arr_turtles.index(t))
+
+# init
+setupScreen()
+createSnakeBody()
+
+doMove()
+
+# closing  eveything
+screen.exitonclick()
