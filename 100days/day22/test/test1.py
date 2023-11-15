5ea35e22817d137b3ada2654507a82a7fb050a03
masterTrooperMx <mastertrooper@gmail.com>
2023-11-14 18:09:40 -0600

    starting pong game

---
+++ 100days/day22/test/test1.py
@@ -0,0 +1,16 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+# constants
+WORLD_WIDTH  = 800
+WOLRD_HEIGHT = 600
+# setup
+screen.setup(width=WORLD_WIDTH, height=WOLRD_HEIGHT)
+screen.bgcolor("black")
+screen.title("PONG Game")
+
+# create screen
+
+
+# closing  eveything
+screen.exitonclick()
