9a0c3f2f8d04aac6bcde766fc470bb4c1fad521a
masterTrooperMx <mastertrooper@gmail.com>
2023-11-21 12:14:30 -0600

    moving around the world

--- 100days/day22/Classes/Ball.py
+++ 100days/day22/Classes/Ball.py
@@ -9,32 +9,46 @@ class Ball(Turtle):
         super().__init__(shape, undobuffersize, visible)
         self.color("white")
         self.angle = -1
+        (self.t_x, self.t_y) = self.pos()
+        self.t_x = Pong.DELTA
+        self.t_y = Pong.DELTA
         
+    def inWorld(self):
+        if self.ycor() < Pong.WORLD_YPOS-1.5*Pong.DELTA and self.ycor() > Pong.WORLD_YNEG+1.5*Pong.DELTA:
+            return True
+        else:
+            return False
+
     def setupBall(self):
         if self.angle < 0:
             self.angle = random.randint(0, 89) # where to go
+            self.setheading(self.angle)
         else:
             # bounce, complementary angle
             self.angle = -1 * self.angle
-        self.setheading(self.angle)
+            self.t_y = -1 * Pong.DELTA
     
     def move(self):
-        (t_x, t_y) = self.pos()
-        if t_y < Pong.WORLD_YPOS-1.5*Pong.DELTA and t_y > Pong.WORLD_YNEG+1.5*Pong.DELTA:
-            self.forward(Pong.DELTA)
+        (new_x, new_y) = self.pos()
+        time.sleep(0.1)
+        #if self.ycor() < Pong.WORLD_YPOS-1.5*Pong.DELTA and self.ycor() > Pong.WORLD_YNEG+1.5*Pong.DELTA:
+        if self.inWorld():
+            #print(f"{Pong.WORLD_YPOS-1.5*Pong.DELTA} vs {Pong.WORLD_YNEG+1.5*Pong.DELTA}")
+            self.goto(self.t_x + new_x, self.t_y + new_y)
         else:
             #print(self.pos(), self.angle)
+            #self.t_y = -1 * Pong.DELTA
             self.setupBall() #bounce
-            self.forward(Pong.DELTA)
+            self.goto(self.t_x + new_x, self.t_y + new_y)
         
-    def moveBall(self):
-        keep_going = True
-        self.setupBall() # turn angle
-        time.sleep(0.1)
-        while keep_going:
-            (t_x, t_y) = self.pos()
-            if t_y >= Pong.WORLD_YNEG+Pong.DELTA:
-                self.forward(Pong.DELTA)
-            else:
-                self.setupBall() #bounce
-                self.forward(Pong.DELTA)
+    #def moveBall(self):
+    #    keep_going = True
+    #    self.setupBall() # turn angle
+    #    time.sleep(0.1)
+    #    while keep_going:
+    #        (t_x, t_y) = self.pos()
+    #        if t_y >= Pong.WORLD_YNEG+Pong.DELTA:
+    #            self.forward(Pong.DELTA)
+    #        else:
+    #            self.setupBall() #bounce
+    #            self.forward(Pong.DELTA)
--- 100days/day22/main.py
+++ 100days/day22/main.py
@@ -5,6 +5,7 @@ from Classes.Ball import *
 
 # setup world and screen
 scoreboard = Scoreboard()
+screen.tracer(0)
 
 r_paddle = Player(Pong.WORLD_XNEG+20, 10)
 l_paddle = Player(Pong.WORLD_XPOS-30, -10)
@@ -22,12 +23,17 @@ screen.onkey(l_paddle.turnDown, 'm')
 game_over = False
 ball.setupBall()
 while not game_over:
-    ball.move()
-    print(f"b{ball.pos()}, lf{l_paddle.pos()}, d{ball.distance(l_paddle)}")
-    if ball.xcor() >= 380 and abs(ball.distance(l_paddle)) < 60:
-        # bounce
-        ball.angle = 270 - ball.angle
-        ball.setupBall()
+    screen.update() # smooth move
+    if ball.inWorld() == True:
+        ball.move()
+        print(f"b{ball.pos()}, lf{l_paddle.pos()}, d{ball.distance(l_paddle)}, {ball.t_x}, {ball.t_y}")
+        if ball.xcor() >= 380 and abs(ball.distance(l_paddle)) < 60:
+            # bounce
+            ball.setupBall()
+    else:
+        print("Ball out!!")
+        game_over = True
+        break
 
 # closing  eveything
 screen.exitonclick()
