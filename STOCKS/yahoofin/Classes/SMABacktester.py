adc4c37dc97134173c75a875bb9f814e4c26a063
masterTrooperMx <mastertrooper@gmail.com>
2023-11-29 14:54:18 -0600

    building up Class Backuptester

--- STOCKS/yahoofin/Classes/SMABacktester.py
+++ STOCKS/yahoofin/Classes/SMABacktester.py
@@ -1,2 +1,9 @@
-class SMABacktester():
-    pass
+import pandas  as pd #Data manipulation
+import numpy as np #Data manipulation
+import yfinance as yf
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-v0_8")
+
+class SMABacktester:
+    def __init__(self) -> None:
+        print("classes!")
---
+++ STOCKS/yahoofin/tests/__init__.py
@@ -0,0 +1,1 @@
+#from SMABacktester import SMABacktester
